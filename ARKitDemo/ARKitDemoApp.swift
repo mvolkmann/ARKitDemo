82a2cc3bbccc1bf6b3ac96af3bb37193b17fb083
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-01-26 11:08:22 -0600

    Initial Commit

---
+++ ARKitDemo/ARKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKitDemoApp.swift
+//  ARKitDemo
+//
+//  Created by Mark Volkmann on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ARKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
